50a7bca5531bf35412c5397873f593798493fa5f
Yuchu Ma <yuchu.ma@berkeley.edu>
2017-09-13 20:51:28 -0700

    finished

--- java/Node.java
+++ java/Node.java
@@ -1,34 +1,63 @@
 class Node {
-  private final int value;
-  private Node left = null;
-  private Node right = null;
+    private final int value;
+    private Node left = null;
+    private Node right = null;
 
-  public Node(int val) {
-    this.value = val;
-  }
+    // Constructs Node.
+    public Node(int val) {
+        this.value = val;
+    }
 
-  public Node right() {
-    return this.right;
-  }
+    public Node right() {
+        return this.right;
+    }
 
-  public Node left() {
-    return this.left;
-  }
+    public Node left() {
+        return this.left;
+    }
 
-  public void setRight(Node r) {
-    this.right = r;
-  }
+    // Sets Node to the right of current Node.
+    public void setRight(Node r) {
+        this.right = r;
+    }
 
-  public void setLeft(Node l) {
-    this.left = l;
-  }
+    // Sets Node to the left of current Node.
+    public void setLeft(Node l) {
+        this.left = l;
+    }
 
-  public int value() {
-    return this.value;
-  }
+    // Returns value of current Node.
+    public int value() {
+        return this.value;
+    }
 
-  public int sumPaths() {
-    // IMPLEMENT ME
-    throw new java.lang.UnsupportedOperationException("Not implemented yet.");
-  }
+    // Returns sum of all numbers created by the
+    // paths from the root to each leaf.
+    public int sumPaths() {
+        return sumPathsHelper(this, 0);
+    }
+
+    // Helper function of sumPaths(), Takes in
+    // current Node NODE, the result RES is set to be 0.
+    public int sumPathsHelper(Node node, int res) {
+        // Base case: null tree.
+        if (node == null) 
+            return -1;
+
+        res = (res * 10 + node.value());
+
+        // Checks if the node is a leaf.
+        if (node.left == null && node.right == null)
+            return res;
+
+        // Recursion for each case to avoid res - 1 when next node is leaf. 
+        if (node.left == null && node.right != null) {
+            return sumPathsHelper(node.right, res);
+        } else if (node.left != null && node.right == null) {
+            return sumPathsHelper(node.left, res);
+        } else {
+            return sumPathsHelper(node.left, res) + sumPathsHelper(node.right, res);
+        }
+    }
+
 }
--- java/SumPaths.java
+++ java/SumPaths.java
@@ -1,19 +1,77 @@
-class SumPaths {
+public class SumPaths {
     public static void main(String[] args) {
-      testSanity();
-      // Write some more test cases
+        testSanity();
+        // Write some more test cases
+        testExampleTree();
+        testExampleTree2();
+        testExampleTree3();
     }
 
     private static void testSanity() {
-      Node root = new Node(1);
+        Node root = new Node(1);
 
-      int res = root.sumPaths();
-      int expected = 1;
+        int res = root.sumPaths();
+        int expected = 1;
 
-      if (res == expected) {
-        System.out.println("Test one passed!");
-      } else {
-        System.out.println("Test one failed; expected " + expected + " but got " + res + " instead.");
-      }
+        if (res == expected) {
+            System.out.println("Test one passed!");
+        } else {
+            System.out.println("Test one failed; expected " + expected + " but got " + res + " instead.");
+        }
+    }
+
+    private static void testExampleTree() {
+        Node root = new Node(1);
+        root.setRight(new Node(5));
+        root.setLeft(new Node(4));
+        root.left().setLeft(new Node(3));
+        root.left().setRight(new Node(7));
+        root.left().right().setRight(new Node(9));
+
+        int res = root.sumPaths();
+        int expected = 1637;
+
+        if (res == expected) {
+            System.out.println("Test Example Tree passed!");
+        } else {
+            System.out.println("Test Example Tree failed; expected " + expected + " but got " + res + " instead.");
+        }
+    }
+
+    // Tree that starts with 0.
+    private static void testExampleTree2() {
+        Node root = new Node(0);
+        root.setLeft(new Node(1));
+        root.setRight(new Node(2));
+        root.left().setLeft(new Node(3));
+        root.left().setRight(new Node(4));
+        root.right().setLeft(new Node(5));
+        root.right().setRight(new Node(6));
+
+        int res = root.sumPaths();
+        int expected = 78;
+
+        if (res == expected) {
+            System.out.println("Test Example Tree 2 passed!");
+        } else {
+            System.out.println("Test Example Tree 2 failed; expected " + expected + " but got " + res + " instead.");
+        }
+    }
+
+    //Tree that ends with 0.
+    private static void testExampleTree3() {
+        Node root = new Node(1);
+        root.setRight(new Node(1));
+        root.right().setLeft(new Node(0));
+        root.right().setRight(new Node(0));
+
+        int res = root.sumPaths();
+        int expected = 220;
+
+        if (res == expected) {
+            System.out.println("Test Example Tree 3 passed!");
+        } else {
+            System.out.println("Test Example Tree 3 failed; expected " + expected + " but got " + res + " instead.");
+        }
     }
 }
